a3a90b6df36be0bdf9f7842db91982ad9b8c0f98
andrew.wan <andrew.wan.crypto@gmail.com>
2024-06-07 22:12:45 +0300

    init

---
+++ init.sh
@@ -0,0 +1,19 @@
+yum install git -y
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache
+yum install docker-ce -y
+
+
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+    "registry-mirrors": [
+        "https://docker.m.daocloud.io",
+        "https://dockerproxy.com",
+        "https://docker.mirrors.ustc.edu.cn",
+        "https://docker.nju.edu.cn"
+    ]
+}
+EOF
+systemctl enable docker
+systemctl start docker
